ace5c0d096e8c5aca46b92aba4e460c021f3e754
Aaron <aaronjens@live.de>
2019-06-04 14:12:54 +0200

    FilterAutor methode Implementiert

--- src/Aufgabe2/LagerTest.java
+++ src/Aufgabe2/LagerTest.java
@@ -8,7 +8,7 @@ import java.util.function.Predicate;
  * Test Klasse fuer das Lager.
  *
  * @author Jan Ehrhardt / Aaron Betzholz
- * @version 03.05.2019
+ * @version 03.06.2019
  */
 public class LagerTest {
     private Lager store = new Lager(21, "HTW Saar");
@@ -17,27 +17,27 @@ public class LagerTest {
         new LagerTest().test();
     }
 
-    private void test(){
+    private void test() {
         store.addArtikel(new Artikel(1000, "Apfel", 1.23, 1));
-        store.addArtikel(new Artikel(1001, "Banane",  1.23, 5));
+        store.addArtikel(new Artikel(1001, "Banane", 1.23, 5));
         store.addArtikel(new Artikel(1002, "Mango", 1.23, 15));
         store.addArtikel(new Artikel(1003, "Kiwi", 1.23, 2));
-        store.addArtikel(new Video(1004, "GoT: Staffel 1",  21.23, 15, "Game of Thrones: Season 1", 300, 2011));
-        store.addArtikel(new CD(1005, "My Little Pony 2",  10.23, 15, "My Little Pony", "Dos", 15));
-        store.addArtikel(new Video(1006, "GoT: Staffel 3",  21.23, 15, "Game of Thrones: Season 3", 300, 2011));
+        store.addArtikel(new Video(1004, "GoT: Staffel 1", 21.23, 15, "Game of Thrones: Season 1", 300, 2011));
+        store.addArtikel(new CD(1005, "My Little Pony 2", 10.23, 15, "My Little Pony", "Dos", 15));
+        store.addArtikel(new Video(1006, "GoT: Staffel 3", 21.23, 15, "Game of Thrones: Season 3", 300, 2011));
         store.addArtikel(new Video(1007, "GoT: Staffel 4", 21.23, 11, "Game of Thrones: Season 4", 300, 2011));
-        store.addArtikel(new Video(1008, "GoT: Staffel 5",  21.23, 15, "Game of Thrones: Season 5", 300, 2011));
+        store.addArtikel(new Video(1008, "GoT: Staffel 5", 21.23, 15, "Game of Thrones: Season 5", 300, 2011));
         store.addArtikel(new Video(1009, "GoT: Staffel 6", 21.23, 15, "Game of Thrones: Season 6", 300, 2011));
-        store.addArtikel(new CD(1010, "My Little Pony",  11.23, 15, "My Little Pony", "Uno", 15));
+        store.addArtikel(new CD(1010, "My Little Pony", 11.23, 15, "My Little Pony", "Uno", 15));
         store.addArtikel(new Video(1011, "GoT: Staffel 8", 21.23, 15, "Game of Thrones: Season 8", 300, 2011));
-        store.addArtikel(new Artikel(1012, "Laptop",  1000.23, 66));
-        store.addArtikel(new Artikel(1013, "Birne",  1.67, 9));
+        store.addArtikel(new Artikel(1012, "Laptop", 1000.23, 66));
+        store.addArtikel(new Artikel(1013, "Birne", 1.67, 9));
         store.addArtikel(new Buch(1014, "Buchzwei", 2.87, 62, "Buchzwei", "Jan", "SOS"));
-        store.addArtikel(new Artikel(1015, "Smartphone",  850.34, 2));
+        store.addArtikel(new Artikel(1015, "Smartphone", 850.34, 2));
         store.addArtikel(new Artikel(1016, "Sonnenbrille", 56.23, 9));
-        store.addArtikel(new Artikel(1017, "Glas",  0.69, 95));
-        store.addArtikel(new Artikel(1018, "T-Shirt",  10.99, 75));
-        store.addArtikel(new Buch(1019, "Bucheins",  59.25, 185, "Bucheins", "Autor", "SES"));
+        store.addArtikel(new Artikel(1017, "Glas", 0.69, 95));
+        store.addArtikel(new Artikel(1018, "T-Shirt", 10.99, 75));
+        store.addArtikel(new Buch(1019, "Bucheins", 59.25, 185, "Bucheins", "Autor", "SES"));
         store.addArtikel(new Artikel(1020, "Pfirsisch", 2.03, 54));
 
         store.applyToSomeArticles(filterBestandZwei, saleFuenf);
@@ -61,9 +61,11 @@ public class LagerTest {
     };
 
     //Aufgabe (h)iii)
-    static Predicate<Buch> filterAutor = t -> {
-        return t.getAutor().equals("Autor");
+    static Predicate<Artikel> filterAutor = t -> {
+        Buch b = (Buch) t;
+        return b.getAutor().equalsIgnoreCase("Autor");
     };
+
 
     //Aufgabe (h)iv)
     /**
@@ -74,7 +76,7 @@ public class LagerTest {
     //Aufgabe (h)v)
     static BiPredicate<Buch, Buch> sortAutor = (t, u) -> {
         int compare = t.getAutor().compareToIgnoreCase(u.getAutor());
-        if (compare < 0){
+        if (compare < 0) {
             return false;
         } else {
             return true;
